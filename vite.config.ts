e362c4b3147a57fa08e86664ff6449602d01a7b2
Igor Schechtel <igorpschechtel@gmail.com>
2026-01-20 23:25:17 -0300

    fix envDir

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,10 @@ import tailwindcss from '@tailwindcss/vite';
 import { devtools } from '@tanstack/devtools-vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
-import { nitro } from 'nitro/vite';
 import { defineConfig } from 'vite';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 const config = defineConfig({
-  envDir: '../',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
